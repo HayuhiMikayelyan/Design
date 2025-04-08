28b46ad97f91c7aed574d9dd1085f06d2e41209c
HayuhiM <hayuhimikayelyan@gmail.com>
2025-04-08 01:46:19 -0800

    Adding resources

--- app/src/main/java/com/example/composedesigncomponents/ui/theme/Color.kt
+++ app/src/main/java/com/example/composedesigncomponents/ui/theme/Color.kt
@@ -9,3 +9,36 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val BrandSecondary50 = Color(0xFFEFEEF6)
+val BrandSecondary200 = Color(0xFFC0BCDC)
+val BrandSecondary600 = Color(0xFF4E4686)
+val BrandSecondary700 = Color(0xFF3A3464)
+val BrandSecondary800 = Color(0xFF272343)
+val BrandSecondary900 = Color(0xFF131121)
+
+val BrandSecondaryBlue50 = Color(0xFFE5F6FF)
+val BrandSecondaryBlue100 = Color(0xFFBBE6FF)
+val BrandSecondaryBlue700 = Color(0xFF00578A)
+val BrandSecondaryBlue800 = Color(0xFF003757)
+
+val Neutral0 = Color(0xFFFFFFFF)
+val Neutral800 = Color(0xFF323135)
+val NeutralDarkDarkest = Color(0xFF1F2024)
+
+val BrandPrimary100 = Color(0xFFFFFACC)
+val BrandPrimary800 = Color(0xFF5C460A)
+
+val Green50 = Color(0xFFECFAF3)
+val Green100 = Color(0xFFC3EEDB)
+val Green200 = Color(0xFFA5E6CA)
+val Green700 = Color(0xFF247E57)
+val Green800 = Color(0xFF115B3A)
+val Green900 = Color(0xFF0F3122)
+
+val Purple25 = Color(0xFFFBF5FF)
+val Purple100 = Color(0xFFE8CCFF)
+val Purple500 = Color(0xFF8E00FF)
+val Purple700 = Color(0xFF550099)
+val Purple800 = Color(0xFF390066)
